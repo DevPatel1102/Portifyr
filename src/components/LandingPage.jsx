cd3c5c65692fd0725e10deb71e153c65d39c474f
KevinBarochiya <aknpb0160b@gmail.com>
2025-04-07 16:20:37 +0530

    Interactive Section added in Landing Page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,12 +1,14 @@
 import { Header } from "./LandingPage/Header";
 import { HeroSection } from "./LandingPage/HeroSection";
 import { Footer } from "./LandingPage/Footer";
+import { InteractivePreviewSection } from "./LandingPage/InteractivePreviewSection";
 
 export const LandingPage = ()=>{
     return(
         <>
             <Header/>
             <HeroSection/>
+            <InteractivePreviewSection/>
             <Footer/>
         </>
     );
